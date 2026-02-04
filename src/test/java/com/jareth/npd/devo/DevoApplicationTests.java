e7f3f22b8d01efbf73efed78125de11fb9d94710
JarethJaziel <jarethmoo@gmail.com>
2026-02-04 01:52:04 -0600

    Setup

---
+++ src/test/java/com/jareth/npd/devo/DevoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jareth.npd.devo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
