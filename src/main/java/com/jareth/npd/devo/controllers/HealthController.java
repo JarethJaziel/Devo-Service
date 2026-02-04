fe979bf0555a9457a040cefea2ae762f8676014b
JarethJaziel <jarethmoo@gmail.com>
2026-02-04 02:06:53 -0600

    Add HealthController

---
+++ src/main/java/com/jareth/npd/devo/controllers/HealthController.java
@@ -0,0 +1,14 @@
+package com.jareth.npd.devo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "App is running!";
+    }
+
+}
